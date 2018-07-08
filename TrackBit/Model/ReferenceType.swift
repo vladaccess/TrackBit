0cf2b9a41bbc1e6e37fe99216945210ae3be5c2d
Vladislav <vladaccess@outlook.com>
2018-07-08 17:50:38 +0300

    added the reference type

---
+++ TrackBit/Model/ReferenceType.swift
@@ -0,0 +1,16 @@
+//
+//  ReferenceType.swift
+//  TrackBit
+//
+//  Created by Vladislav on 08.07.2018.
+//  Copyright © 2018 Vladislav. All rights reserved.
+//
+
+import Foundation
+
+enum ReferenceType:String {
+    case week = "Week"
+    case month = "Month"
+    case year = "Year"
+    case all = "All"
+}
