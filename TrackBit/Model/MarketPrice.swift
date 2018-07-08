d6b25d7ddfce99ecfc288bc9af4cc78cf88e4205
Vladislav <vladaccess@outlook.com>
2018-07-08 20:06:19 +0300

    added marketPrice

---
+++ TrackBit/Model/MarketPrice.swift
@@ -0,0 +1,25 @@
+//
+//  MarketPrice.swift
+//  TrackBit
+//
+//  Created by Vladislav on 08.07.2018.
+//  Copyright © 2018 Vladislav. All rights reserved.
+//
+
+import Foundation
+
+struct MarketPrice:Decodable {
+    
+    let values:[Value]
+    
+}
+
+struct Value:Decodable {
+    let axisX:Int
+    let axisY:Float
+}
+
+enum CodingKeys:String,CodingKey {
+    case axisX = "x"
+    case axisY = "y"
+}
